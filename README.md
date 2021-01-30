1674a51b11af10370a5c2960a2c2ed094711ef9a
nmcgann <neil@neilmcgann.co.uk>
2021-01-30 10:25:27 +0000

    Initial commit of modified version

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Web Audio Metronome
 
+NM modified version that displays selectable cycle of 4ths or random notes on the first beat of the bar. Also has a mute button.
+
 This application shows how to use a collaboration between a setTimeout scheduler and the Web Audio scheduler to properly implement rock-solid timing for audio applications.
 
 Check it out, feel free to submit issues or requests, fork, submit pull requests, etc.
--- js/metronome.js
+++ js/metronome.js
@@ -1,3 +1,5 @@
+//metronome.js
+//
 var audioContext = null;
 var unlocked = false;
 var isPlaying = false;      // Are we currently playing?
@@ -10,7 +12,7 @@ var scheduleAheadTime = 0.1;    // How far ahead to schedule audio (sec)
                             // This is calculated from lookahead, and overlaps 
                             // with next interval (in case the timer is late)
 var nextNoteTime = 0.0;     // when the next note is due.
-var noteResolution = 0;     // 0 == 16th, 1 == 8th, 2 == quarter note
+var noteResolution = 0;     // 2 == 16th, 1 == 8th, 0 == quarter note **NM changed 16th and quarter
 var noteLength = 0.05;      // length of "beep" (in seconds)
 var canvas,                 // the canvas element
     canvasContext;          // canvasContext is the canvas' context 2D
@@ -18,7 +20,15 @@ var last16thNoteDrawn = -1; // the last "box" we drew on the screen
 var notesInQueue = [];      // the notes that have been put into the web audio,
                             // and may or may not have played yet. {note, time}
 var timerWorker = null;     // The Web Worker used to fire timer messages
+//NM
+var noteDisplay;			//the note display element
+var notesCyc4 = ["C","F","Bb","Eb","Ab","Db","Gb","B","E","A","D","G"];
+var notesRand = ["C","F","Bb/A#","Eb/D#","Ab/G#","Db/C#","Gb/F#","B","E","A","D","G"]; //notesCyc4.slice(0);
+var noteCount = 0;
 
+var sequence = 0; 			// 0=none,1=cycle of 4ths, 2=random
+var isMuted = false;      	// Are we currently muted?
+var gainNode = null;
 
 // First, let's shim the requestAnimationFrame API, with a setTimeout fallback
 window.requestAnimFrame = (function(){
@@ -50,12 +60,20 @@ function scheduleNote( beatNumber, time ) {
 
     if ( (noteResolution==1) && (beatNumber%2))
         return; // we're not playing non-8th 16th notes
-    if ( (noteResolution==2) && (beatNumber%4))
+    if ( (noteResolution==0) && (beatNumber%4))
         return; // we're not playing non-quarter 8th notes
 
     // create an oscillator
     var osc = audioContext.createOscillator();
-    osc.connect( audioContext.destination );
+    //osc.connect( audioContext.destination );
+	
+//NM added mute control
+	// Connect the source to the gain node.
+	osc.connect(gainNode);
+	// Connect the gain node to the destination.
+	gainNode.connect(audioContext.destination);
+
+	
     if (beatNumber % 16 === 0)    // beat 0 == high pitch
         osc.frequency.value = 880.0;
     else if (beatNumber % 4 === 0 )    // quarter notes = medium pitch
@@ -91,7 +109,14 @@ function play() {
     if (isPlaying) { // start playing
         current16thNote = 0;
         nextNoteTime = audioContext.currentTime;
+		//console.log(nextNoteTime);
+		nextNoteTime += 0.1; //NM added to fix problem with first note being clipped
+		
         timerWorker.postMessage("start");
+		
+		//NM added
+		noteCount = 0;
+		//
         return "stop";
     } else {
         timerWorker.postMessage("stop");
@@ -124,13 +149,56 @@ function draw() {
         for (var i=0; i<16; i++) {
             canvasContext.fillStyle = ( currentNote == i ) ? 
                 ((currentNote%4 === 0)?"red":"blue") : "black";
+				
             canvasContext.fillRect( x * (i+1), x, x/2, x/2 );
         }
         last16thNoteDrawn = currentNote;
+
+//NM added here
+		//change key displayed on first beat only
+		if(currentNote === 0){
+			if(sequence === 1){ //cyc 4ths
+				noteDisplay.innerHTML = notesCyc4[noteCount];				
+			} else if (sequence === 2){ //random
+				noteDisplay.innerHTML = notesRand[noteCount];
+			} else { //none
+				noteDisplay.innerHTML = "";
+			}
+
+			if(noteCount++ >= notesCyc4.length) { //all sequences same length
+				noteCount = 0;
+				shuffleArray(notesRand);
+				//console.log(notesRand);
+			}	
+		}
+//		
     }
 
     // set up to draw again
     requestAnimFrame(draw);
+	
+}
+
+//NM added - off stackexchange, a random shuffle in place function
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
+//NM added
+function mute() { //mute event  - zeros out the gain node, or sets to full depending on state
+    isMuted = !isMuted;
+
+    if (isMuted) {
+ 		gainNode.gain.value = 0;
+		return "unmute";
+    } else {
+		gainNode.gain.value = 1;
+		return "mute";
+    }
 }
 
 function init(){
@@ -140,11 +208,29 @@ function init(){
     canvas = document.createElement( 'canvas' );
     canvasContext = canvas.getContext( '2d' );
     canvas.width = window.innerWidth; 
-    canvas.height = window.innerHeight; 
+    canvas.height = window.innerHeight / 3; 
     document.body.appendChild( container );
     container.appendChild(canvas);    
     canvasContext.strokeStyle = "#ffffff";
     canvasContext.lineWidth = 2;
+
+//NM new
+    noteDisplay = document.createElement( 'div' );
+    noteDisplay.className = "notedisplay";
+    document.body.appendChild( noteDisplay );
+
+	noteDisplay.innerHTML = "";
+	
+	shuffleArray(notesRand);
+	
+	var element1 = document.getElementById('resSelect');
+	element1.selectedIndex  = 0;
+	element1 = document.getElementById('seqSelect');
+	element1.selectedIndex  = 0;
+	element1 = document.getElementById('tempo');	
+	element1.value = tempo;
+	
+//
 
     // NOTE: THIS RELIES ON THE MONKEYPATCH LIBRARY BEING LOADED FROM
     // Http://cwilso.github.io/AudioContext-MonkeyPatch/AudioContextMonkeyPatch.js
@@ -154,6 +240,11 @@ function init(){
     audioContext = new AudioContext();
 
     // if we wanted to load audio files, etc., this is where we should do it.
+
+//NM added mute(vol) control
+	// Create a gain node.
+	gainNode = audioContext.createGain();
+	gainNode.gain.value = 1; //default to full
 
     window.onorientationchange = resetCanvas;
     window.onresize = resetCanvas;
